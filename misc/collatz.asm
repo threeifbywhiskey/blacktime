c0cb11092efd81fcbe972747e9e45de1e9e437da
Three If By Whiskey <3ifbyw@gmail.com>
2014-02-27 20:43:39 -0500

    Add Collatz sequence procedure

---
+++ misc/collatz.asm
@@ -0,0 +1,13 @@
+push 0 dup inum load ; Read and load a number.
+
+0:
+    dup onum push 32 ochr        ; Print the number and a space.
+    dup push 1 sub jz 2          ; Exit when the number is 1.
+    dup push 2 mod jz 1          ; Jump to handle even.
+    push 3 mul push 1 add jump 0 ; 3n + 1 if odd
+
+1:
+    push 2 div jump 0            ; Divide by 2 and go again.
+
+2:
+    push 10 ochr exit            ; Print a newline and exit.
