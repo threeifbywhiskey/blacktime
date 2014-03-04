b496e7cd18487e2a27c158a837dc6ec9e5384671
Three If By Whiskey <3ifbyw@gmail.com>
2014-03-04 15:00:57 -0500

    Improve Collatz procedure alignment

--- misc/collatz.asm
+++ misc/collatz.asm
@@ -1,13 +1,14 @@
-push 0 dup inum load ; Read and load a number.
+push 0 dup inum load      ; Read and load a number.
 
 0:
-    dup onum push 32 ochr        ; Print the number and a space.
-    dup push 1 sub jz 2          ; Exit when the number is 1.
-    dup push 2 mod jz 1          ; Jump to handle even.
-    push 3 mul push 1 add jump 0 ; 3n + 1 if odd
+    dup onum push 32 ochr ; Print the number and a space.
+    dup push 1 sub jz 2   ; Exit when the number is 1.
+    dup push 2 mod jz 1   ; Jump to handle even.
+    push 3 mul push 1 add ; 3n + 1 if odd
+    jump 0 
 
 1:
-    push 2 div jump 0            ; Divide by 2 and go again.
+    push 2 div jump 0     ; Divide by 2 and go again.
 
 2:
-    push 10 ochr exit            ; Print a newline and exit.
+    push 10 ochr exit     ; Print a newline and exit.
